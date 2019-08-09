e456a3160e2fa61a452b011d527dc80cfbede622
ribbiteli <ribbiteli@gmail.com>
2019-08-09 10:40:04 -0400

    completed and working!

--- converter.js
+++ converter.js
@@ -6,78 +6,78 @@ initialJamo = new Object();
 
 initialJamo.r = 0, // ㄱ
 initialJamo.R = 1, // ㄲ
-initialJamo.s = initialJamo.S = 2, // ㄴ
+initialJamo.S = initialJamo.s = 2, // ㄴ
 initialJamo.e = 3, // ㄷ
 initialJamo.E = 4, // ㄸ
-initialJamo.f = initialJamo.F = 5, // ㄹ
-initialJamo.a = initialJamo.A = 6, // ㅁ
+initialJamo.F = initialJamo.f = 5, // ㄹ
+initialJamo.A = initialJamo.a = 6, // ㅁ
 initialJamo.q = 7, // ㅂ
 initialJamo.Q = 8, // ㅃ
 initialJamo.t = 9, // ㅅ
 initialJamo.T = 10, // ㅆ
-initialJamo.d = initialJamo.D = 11, // ㅇ
+initialJamo.D = initialJamo.d = 11, // ㅇ
 initialJamo.w = 12, // ㅈ
 initialJamo.W = 13, // ㅉ
-initialJamo.c = initialJamo.C = 14, // ㅊ
-initialJamo.z = initialJamo.Z = 15, // ㅋ
-initialJamo.x = initialJamo.X = 16, // ㅌ
-initialJamo.v = initialJamo.V = 17, // ㅍ
-initialJamo.g = initialJamo.G = 18; // ㅎ
+initialJamo.C = initialJamo.c = 14, // ㅊ
+initialJamo.Z = initialJamo.z = 15, // ㅋ
+initialJamo.X = initialJamo.x = 16, // ㅌ
+initialJamo.V = initialJamo.v = 17, // ㅍ
+initialJamo.G = initialJamo.g = 18; // ㅎ
 // the letters unused are the vowels yuiophjklbnm = ㅛㅕㅑㅐㅔㅗㅓㅏㅣㅠㅜㅡ
 
 medialJamo = new Object();
 
-medialJamo.k = medialJamo.K = 0, // ㅏ
+medialJamo.K = medialJamo.k = 0, // ㅏ
 medialJamo.o = 1, // ㅐ
-medialJamo.i = medialJamo.I = 2, // ㅑ
+medialJamo.I = medialJamo.i = 2, // ㅑ
 medialJamo.O = 3, // ㅒ
-medialJamo.j = medialJamo.J = 4, // ㅓ 
+medialJamo.J = medialJamo.j = 4, // ㅓ 
 medialJamo.p = 5, // ㅔ
-medialJamo.u = medialJamo.U = 6, // ㅕ
+medialJamo.U = medialJamo.u = 6, // ㅕ
 medialJamo.P = 7, // ㅖ
-medialJamo.h = medialJamo.H = 8, // ㅗ
-medialJamo.hk = medialJamo.Hk = medialJamo.HK = medialJamo.hK = 9, // ㅘ
-medialJamo.ho = medialJamo.Ho = 10, // ㅙ
-medialJamo.hl = medialJamo.Hl = medialJamo.HL = medialJamo.hL = 11, // ㅚ 
-medialJamo.y = medialJamo.Y = 12, // ㅛ 
-medialJamo.n = medialJamo.N = 13, //ㅠㅜ 
-medialJamo.nj = medialJamo.Nj = medialJamo.NJ = medialJamo.nJ = 14, // ㅝ
-medialJamo.np = medialJamo.Np = 15, // ㅞ
-medialJamo.nl = medialJamo.Nl = medialJamo.NL = medialJamo.nL = 16, // ㅟ
-medialJamo.b = medialJamo.B = 17, // ㅠ
-medialJamo.m = medialJamo.M = 18, // ㅡ
-medialJamo.ml = medialJamo.Ml = medialJamo.ML = medialJamo.ML = 19, // ㅢ
-medialJamo.l = medialJamo.L = 20; // ㅣ
+medialJamo.H = medialJamo.h = 8, // ㅗ
+medialJamo.hK = medialJamo.Hk = medialJamo.HK = medialJamo.hk = 9, // ㅘ
+medialJamo.Ho = medialJamo.ho = 10, // ㅙ
+medialJamo.hL = medialJamo.Hl = medialJamo.HL = medialJamo.hl = 11, // ㅚ 
+medialJamo.Y = medialJamo.y = 12, // ㅛ 
+medialJamo.N = medialJamo.n = 13, //ㅠㅜ 
+medialJamo.nJ = medialJamo.Nj = medialJamo.NJ = medialJamo.nj = 14, // ㅝ
+medialJamo.Np = medialJamo.np = 15, // ㅞ
+medialJamo.nL = medialJamo.Nl = medialJamo.NL = medialJamo.nl = 16, // ㅟ
+medialJamo.B = medialJamo.b = 17, // ㅠ
+medialJamo.M = medialJamo.m = 18, // ㅡ
+medialJamo.mL = medialJamo.Ml = medialJamo.ML = medialJamo.ml = 19, // ㅢ
+medialJamo.L = medialJamo.l = 20; // ㅣ
 
 finalJamo = new Object();
 // none = 0
 finalJamo.r = 1, // ㄱ
 finalJamo.R = 2, // ㄲ
 finalJamo.rt = 3, // ㄳ 
-finalJamo.s = finalJamo.S = 4, // ㄴ
-finalJamo.sw = finalJamo.Sw = 5, // ㄵ
-finalJamo.sg = finalJamo.Sg = finalJamo.SG = finalJamo.sG = 6, // ㄶ 
+finalJamo.S = finalJamo.s = 4, // ㄴ
+finalJamo.Sw = finalJamo.sw = 5, // ㄵ
+finalJamo.sG = finalJamo.Sg = finalJamo.SG = finalJamo.sg = 6, // ㄶ 
 finalJamo.e = 7, // ㄷ
-finalJamo.f = finalJamo.F = 8, // ㄹ
-finalJamo.fr = finalJamo.Fr = 9, // ㄺ 
-finalJamo.fa = finalJamo.Fa = finalJamo.FA = finalJamo.fA = 10, // ㄻ 
-finalJamo.fq = finalJamo.Fq = 11, // ㄼ 
-finalJamo.ft = finalJamo.Ft = 12, // ㄽ 
-finalJamo.fx = finalJamo.Fx = finalJamo.FX = finalJamo.fX = 13, // ㄾ 
-finalJamo.fv = finalJamo.Fv = finalJamo.FV = finalJamo.fV = 14, // ㄿ
-finalJamo.fg = finalJamo.Fg = finalJamo.FG = finalJamo.fG = 15, // ㅀ
-finalJamo.a = finalJamo.A = 16, // ㅁ
+finalJamo.F = finalJamo.f = 8, // ㄹ
+finalJamo.Fr = finalJamo.fr = 9, // ㄺ 
+finalJamo.fA = finalJamo.Fa = finalJamo.FA = finalJamo.fa = 10, // ㄻ 
+finalJamo.Fq = finalJamo.fq = 11, // ㄼ 
+finalJamo.Ft = finalJamo.ft = 12, // ㄽ 
+finalJamo.fX = finalJamo.Fx = finalJamo.FX = finalJamo.fx = 13, // ㄾ 
+finalJamo.fV = finalJamo.Fv = finalJamo.FV = finalJamo.fv = 14, // ㄿ
+finalJamo.fG = finalJamo.Fg = finalJamo.FG = finalJamo.fg = 15, // ㅀ
+finalJamo.A = finalJamo.a = 16, // ㅁ
 finalJamo.q = 17, // ㅂ
 finalJamo.qt = 18, // ㅄ
 finalJamo.t = 19, // ㅅ
 finalJamo.T = 20, // ㅆ
-finalJamo.d = finalJamo.D = 21, // ㅇ 
+finalJamo.D = finalJamo.d = 21, // ㅇ 
 finalJamo.w = 22, // ㅈ
-finalJamo.c = finalJamo.C = 23, // ㅊ
-finalJamo.z = finalJamo.Z = 24, // ㅋ
-finalJamo.x = finalJamo.X = 25, // ㅌ 
-finalJamo.v = finalJamo.V = 26, // ㅍ
-finalJamo.g = finalJamo.G = 27; // ㅎ
+finalJamo.C = finalJamo.c = 23, // ㅊ
+finalJamo.Z = finalJamo.z = 24, // ㅋ
+finalJamo.X = finalJamo.x = 25, // ㅌ 
+finalJamo.V = finalJamo.v = 26, // ㅍ
+finalJamo.G = finalJamo.g = 27; // ㅎ
 
 linearFullCode = new Object();
 
@@ -91,41 +91,42 @@ linearFullCode.r = 12593, // ㄱ
 linearFullCode.R = 12594, // ㄲ
 linearFullCode.t = 12613, // ㅅ
 linearFullCode.T = 12614, // ㅆ
-linearFullCode.y = linearFullCode.Y = 12635, // ㅛ
-linearFullCode.u = linearFullCode.U = 12629, // ㅕ
-linearFullCode.i = linearFullCode.I = 12625, // ㅑ
+linearFullCode.Y = linearFullCode.y = 12635, // ㅛ
+linearFullCode.U = linearFullCode.u = 12629, // ㅕ
+linearFullCode.I = linearFullCode.i = 12625, // ㅑ
 linearFullCode.o = 12624, // ㅐ
 linearFullCode.O = 12626, // ㅒ
 linearFullCode.p = 12628, // ㅔ
 linearFullCode.P = 12630, // ㅖ
-linearFullCode.a = linearFullCode.A = 12609, // ㅁ
-linearFullCode.s = linearFullCode.S = 12596, // ㄴ
-linearFullCode.d = linearFullCode.D = 12615, // ㅇ
-linearFullCode.f = linearFullCode.F = 12601, // ㄹ
-linearFullCode.g = linearFullCode.G = 12622, // ㅎ
-linearFullCode.h = linearFullCode.H = 12631, // ㅗ
-linearFullCode.hk = linearFullCode.Hk 
-= linearFullCode.HK = linearFullCode.hK = 12632, // ㅘ
-linearFullCode.ho = linearFullCode.Ho = 12633, // ㅙ
-linearFullCode.hl = linearFullCode.Hl 
-= linearFullCode.HL = linearFullCode.hL = 12634, // ㅚ 
-linearFullCode.j = linearFullCode.J = 12627, // ㅓ
-linearFullCode.k = linearFullCode.K = 12623, // ㅏ
-linearFullCode.l = linearFullCode.L = 12643, // ㅣ
-linearFullCode.z = linearFullCode.Z = 12619, // ㅋ
-linearFullCode.x = linearFullCode.X = 12620, // ㅌ
-linearFullCode.c = linearFullCode.C = 12618, // ㅊ
-linearFullCode.v = linearFullCode.V = 12621, // ㅍ
-linearFullCode.b = linearFullCode.B = 12640, // ㅠ
-linearFullCode.n = linearFullCode.N = 12636, //ㅠㅜ
-linearFullCode.nj = linearFullCode.Nj 
-= linearFullCode.NJ = linearFullCode.nJ = 12637, // ㅝ
-linearFullCode.np = linearFullCode.Np = 12638, // ㅞ
-linearFullCode.nl = linearFullCode.Nl 
-= linearFullCode.NL = linearFullCode.nL = 12639, // ㅟ
-linearFullCode.m = linearFullCode.M = 12641, // ㅡ
-linearFullCode.ml = linearFullCode.Ml 
-= linearFullCode.ML = linearFullCode.ML = 12642; // ㅢ
+linearFullCode.A = linearFullCode.a = 12609, // ㅁ
+linearFullCode.S = linearFullCode.s = 12596, // ㄴ
+linearFullCode.D = linearFullCode.d = 12615, // ㅇ
+linearFullCode.F = linearFullCode.f = 12601, // ㄹ
+linearFullCode.G = linearFullCode.g = 12622, // ㅎ
+linearFullCode.H = linearFullCode.h = 12631, // ㅗ
+linearFullCode.hK = linearFullCode.Hk 
+= linearFullCode.HK = linearFullCode.hk = 12632, // ㅘ
+linearFullCode.Ho = linearFullCode.ho = 12633, // ㅙ
+linearFullCode.hL = linearFullCode.Hl 
+= linearFullCode.HL = linearFullCode.hl = 12634, // ㅚ 
+linearFullCode.J = linearFullCode.j = 12627, // ㅓ
+linearFullCode.K = linearFullCode.k = 12623, // ㅏ
+linearFullCode.L = linearFullCode.l = 12643, // ㅣ
+linearFullCode.Z = linearFullCode.z = 12619, // ㅋ
+linearFullCode.X = linearFullCode.x = 12620, // ㅌ
+linearFullCode.C = linearFullCode.c = 12618, // ㅊ
+linearFullCode.V = linearFullCode.v = 12621, // ㅍ
+linearFullCode.B = linearFullCode.b = 12640, // ㅠ
+linearFullCode.N = linearFullCode.n = 12636, //ㅠㅜ
+linearFullCode.nJ = linearFullCode.Nj 
+= linearFullCode.NJ = linearFullCode.nj = 12637, // ㅝ
+linearFullCode.Np = linearFullCode.np = 12638, // ㅞ
+linearFullCode.nL = linearFullCode.Nl 
+= linearFullCode.NL = linearFullCode.nl = 12639, // ㅟ
+linearFullCode.M = linearFullCode.m = 12641, // ㅡ
+linearFullCode.mL = linearFullCode.Ml 
+= linearFullCode.ML = linearFullCode.ml = 12642; // ㅢ
+linearFullCode.rt = 12595
 
 
 // The precomposed hangul syllables in the Hangul Syllables block 
@@ -223,14 +224,15 @@ function blockBreaker(text) {
   var finalJamoValue = -1;
   var codeNum = 0;
   while (i < text.length) {
-    codeNum = String.prototype.charCodeAt(i);
-    if (12610 <= codeNum <= 12642) {
-      str += findKey(linearFullCode, codeNum);
+    codeNum = text.charCodeAt(i);
+    if (12593 <= codeNum && codeNum <= 12643) {
+      str += findKey(linearFullCode, codeNum); // need to make double qwert double letter (option?)
       i += 1;
-    } else if (0 <= codeNum - 44032 <= 11171) {
-      initialJamoValue = floor((codeNum - 44032) / 588);
+    } else if (44032 <= codeNum && codeNum <= 55203) {
+      initialJamoValue = Math.floor((codeNum - 44032) / 588);
+      var ans = 0;
       for (j = 0, filled = false; j < 28 && !filled; j++) {
-        var ans = (codeNum - 44032 - initialJamoValue * 588 - j) / 28;
+        ans = ((codeNum - 44032 - j) - (initialJamoValue * 588)) / 28;
         if (Number.isInteger(ans)) {
           medialJamoValue = ans;
           finalJamoValue = j;
@@ -251,4 +253,4 @@ function blockBreaker(text) {
   return str;
 }
 // Currently all characters are output as undefined INCL. strings
-// passed directly
+// passed directly. except linear 
